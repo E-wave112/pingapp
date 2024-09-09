71d136f6170e173c80c3d79b440b9686d31807ec
Osagie Iyayi <osagieiyayi09@gmail.com>
2024-09-09 23:55:35 +0100

    updated app.js base.css and index.html file

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-const formEl = document.form1.input1
-const Btn = document.querySelector('.btn-primary');
-const errMsg = document.querySelector('.error-msg');
-const mailformat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/
+const formEl = document.form1.input1;
+const Btn = document.querySelector(".btn-primary");
+const errMsg = document.querySelector(".error-msg");
+const mailformat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
 
-Btn.addEventListener('click',(e)=>{
-    e.preventDefault();
-    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(formEl.value)){
-        errMsg.textContent = 'validated successfully !'
-        errMsg.style.color = 'black';
-        return true
-    }else if (formEl.value === ''){
-        errMsg.textContent = 'field cannnot be blank !'
-        errMsg.style.color = 'red'
-        //formEl.focus();
-        return false
-    }else {
-        errMsg.textContent = 'Please enter a valid email address!';
-        errMsg.style.color = 'red';
-        //formEl.focus();
-        return false
-    }
-    
-})
+Btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+      formEl.value
+    )
+  ) {
+    errMsg.textContent = "validated successfully !";
+    errMsg.style.color = "black";
+    return true;
+  } else if (formEl.value === "") {
+    errMsg.textContent = "field cannnot be blank !";
+    errMsg.style.color = "red";
+    //formEl.focus();
+    return false;
+  } else {
+    errMsg.textContent = "Please enter a valid email address!";
+    errMsg.style.color = "red";
+    //formEl.focus();
+    return false;
+  }
+});
